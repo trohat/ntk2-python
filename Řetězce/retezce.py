397bc6a04f2bb895d0496212ed3812f191806124
Trohat <petr.sed@centrum.cz>
2023-10-10 15:55:11 +0200

    Retezce

---
+++ Řetězce/retezce.py
@@ -0,0 +1,25 @@
+pozdrav = "ahoj Marku"
+
+pozdrav.upper()
+"ahoj Marku".upper()
+pozdrav.lower()
+
+dlouhy_pozdrav = "ahoj Marku, jak se máš? Co budeš dnes večer dělat?"
+
+dlouhy_pozdrav.title()
+dlouhy_pozdrav.capitalize()
+
+dlouhy_pozdrav = dlouhy_pozdrav.upper()
+
+dlouhy_pozdrav.index("AHOJ")
+dlouhy_pozdrav.find("AHOJ")
+
+"""
+mereni053=45,78,12,69,12
+"""
+vysledek = 5000
+
+print("Daně, které musíš zaplatit, jsou ve výši " + str(vysledek) + " Kč.")
+print(f"Daně, které musíš zaplatit, jsou ve výši {vysledek} Kč.")
+print(f"Dvakrát sedm je {2 * 7}, věříš mi to?")
+
